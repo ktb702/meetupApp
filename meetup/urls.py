1122c48d3a7c38cb32b2ffe84e1fe279b4742027
ktb702 <59974874+ktb702@users.noreply.github.com>
2021-01-20 10:33:08 -0700

    Add files via upload

---
+++ meetup/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path, include
+from .views import *
+from meetup import views
+
+# Various Urls we will be using for the website
+urlpatterns=[
+    path('', views.index, name='index'),
+
+    path('login/', views.login, name='login'),
+    path('getmeetup/', views.getmeetup, name='meetup'),
+    path('getactivity/', views.getactivity, name='activity'),
+    path('meetupdetails/<int:id>',views.meetupdetails, name='meetupdetails'),
+    path('newmeetup/', views.newmeetup, name='newmeetup'),
+    path('newactivity/', views.newactivity, name='newactivity'),
+    path('loginmessage/', views.loginmessage, name='loginmessage'),
+    path('logoutmessage/', views.logoutmessage, name='logoutmessage'),   
+
+]
